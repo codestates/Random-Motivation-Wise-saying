29f3f3511058e8e09b0196f3246dc8c7d51ed2b5
JANGto1 <dadadaca12@gmail.com>
2022-05-16 17:44:13 +0900

    client 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,13 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import { Counter } from './features/counter/Counter';
->>>>>>> 4f2726682180ccc1045fba58f291b0033acb03c7
 import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className='App'>
       <header className='head'>
         <h2 className='home'>Home</h2>
         <h3 className='MyPage'>MyPage</h3>
-        </header>
-=======
-    <div className="App">
-      <header className="App-header">
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
->>>>>>> 4f2726682180ccc1045fba58f291b0033acb03c7
     </div>
   );
 }
