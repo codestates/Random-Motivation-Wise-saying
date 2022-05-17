81e0c7621065da0f80a0ec311aea97eed74a3f46
mj <mj577900@gmail.com>
2022-05-17 20:23:07 +0900

    Change login,signout,signup

--- server/controllers/users/signout.js
+++ server/controllers/users/signout.js
@@ -1,10 +1,13 @@
 /** 로그아웃 기능 */
 module.exports = (req, res) => {
-    if(req.cookies.jwt) {
+    if(req.cookies) {
+        res.cookie('token', null, {
+            maxAge: 0
+        })
         res.json({message: 'Success logout'})
-    }
-    if(!req.cookies.jwt) {
+    } else if(!req.cookies) {
         res.status(400).json({message: 'You currently not logined'})
+    } else {
+        res.status(500).json({message: 'Server error'})
     }
-    res.status(500).json({message: 'Server error'})
 }
--- server/index.js
+++ server/index.js
@@ -2,10 +2,17 @@ const express = require("express"); // express 모듈을 가져와서
 const app = express(); // 새로운 express앱을 생성
 const port = 8080;
 const controllers = require("./controllers")
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json()); /** 클라이언트 body 해석 위함 */
+app.use(express.urlencoded({ extended: false })); /** 클라이언트 body 해석 위함 */
 
 app.get("/", (req, res) => res.json("Hi Claire Welcome to Node.js")); // 여기에 나중에 HTML을 넣어주게 된다.
-app.post("/signup", controllers.signup);
-app.post("/login", controllers.login);
-app.post("/signout", controllers.signout);
+// app.get("/:userId", controllers.userinfo);
+
+app.post("/users/signup", controllers.signup);
+app.post("/users/login", controllers.login);
+app.post("/users/signout", controllers.signout);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
